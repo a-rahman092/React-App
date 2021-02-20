ff90ff3b06a3a8ea9a217cb8e5127f09f376bc7f
a-rahman092 <72246399+a-rahman092@users.noreply.github.com>
2021-02-20 22:43:54 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "bootstrap-css-only/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+class App extends Component {
+    state = {};
+    render() {
+        return (
+            <React.Fragment>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Features</a>
+      <a class="nav-item nav-link" href="#">Pricing</a>
+      <a class="nav-item nav-link disabled" href="#">Disabled</a>
+    </div>
+  </div>
+</nav>
+            </React.Fragment>
+        );
+    }
+}
+ReactDOM.render(<App />, document.getElementById("root"));
+
